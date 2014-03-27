9b6cdad2350b6913eee4ec76ba111726eb6ddb9c
Scarpen <scarpen@scarpen-virtual-machine.(none)>
2014-03-27 13:47:05 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SisconOtica::Application.config.secret_key_base = '883ca12f82a6ae0e3904acdf7944f59976421fca86c8b404ce941e5fc05766eb1c61877fd3cf27970db43c2b21a2964dee033aaa89e6f3fbe987c94dd1d2ee26'
